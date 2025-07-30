55ec692c340307a2a7e3b7ed956538dedc5df417
DuYicheng <duyicheng741@outlook.com>
2025-07-30 22:21:07 +0800

    调整测试的位置，重构测试的逻辑。完全完成了从 rmdev 到 emdevif 的迁移

---
+++ test/test_main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file test_main.cpp
+ * @author DuYicheng
+ * @date 2025-07-30
+ * @brief 测试主函数
+ */
+
+#include "test_main.hpp"
+
+extern "C" void emdevif_testEntry(void)
+{
+    initOnceTest();
+    arrayTraitsAggregateTest();
+}
---
+++ test/test_main.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file test_main.hpp
+ * @author DuYicheng
+ * @date 2025-07-30
+ * @brief 测试主函数
+ */
+ 
+#pragma once
+#ifndef EMDEVIF_TEST_MAIN_HPP
+#define EMDEVIF_TEST_MAIN_HPP
+
+void initOnceTest();
+void arrayTraitsAggregateTest();
+
+#endif // !EMDEVIF_TEST_MAIN_HPP
