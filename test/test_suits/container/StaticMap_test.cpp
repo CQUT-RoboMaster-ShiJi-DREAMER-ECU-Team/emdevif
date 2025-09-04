8b7453b939762444a93aac17494d78a6c19bf9cb
DuYicheng <duyicheng741@outlook.com>
2025-09-04 22:18:04 +0800

    增加 StaticMap 的测试

--- test/test_suits/container/StaticMap_test.cpp
+++ test/test_suits/container/StaticMap_test.cpp
@@ -5,6 +5,8 @@
  * @brief 静态键值对测试
  */
 
+#include <string_view>
+
 import emdevif.container.Map;
 
 using emdevif::StaticMap;
@@ -16,3 +18,10 @@ static_assert(map1.at(9) == nullptr);
 static_assert(*map1.at(2) == 'b');
 static_assert(map1[2] == 'b');
 static_assert(map1[114514] == 'k');
+
+static constexpr auto map2 =
+    emdevif::makeStaticMap<std::string_view, int>({{"a", 1}, {"abc", 2}, {"cdef", 3}, {"qwert", 1244}});
+
+static_assert(map2.find("none") == map2.cend());
+static_assert(map2["a"] == 1);
+static_assert(map2["qwert"] == 1244);
