55266c9dc576e16580d4c1df19895b760a5e53f2
DuYicheng <duyicheng741@outlook.com>
2025-09-29 14:55:47 +0800

    重构日志的测试

--- test/test_suits/logger/Logger_test.cpp
+++ test/test_suits/logger/Logger_test.cpp
@@ -11,22 +11,22 @@
 
 import emdevif.logger;
 import emdevif.userDeclares;
+import emdevif.sys.thread;
 
 #define EMDEVIF_LOGGER_TEST_FILE_NAME "Logger_test.cpp"
 
 // #define EMDEVIF_LOGGER_TEST_DEVELOPER_CHECK_MODE
 
 #ifdef EMDEVIF_LOGGER_TEST_DEVELOPER_CHECK_MODE
-#define EMDEVIF_LOGGER_TEST_LOG(expected, log_level, format, ...)    \
+#define EMDEVIF_LOGGER_TEST_LOG(log_level, format, ...)              \
     do {                                                             \
         EMDEVIF_LOG(log_level, format __VA_OPT__(, ) __VA_ARGS__);   \
         TEST_LOG("%s", emdevif::user_declares::logger::getBuffer()); \
     } while (0)
 #else
-#define EMDEVIF_LOGGER_TEST_LOG(expected, log_level, format, ...)            \
-    do {                                                                     \
-        EMDEVIF_LOG(log_level, format __VA_OPT__(, ) __VA_ARGS__);           \
-        EXPECT_STREQ(expected, emdevif::user_declares::logger::getBuffer()); \
+#define EMDEVIF_LOGGER_TEST_LOG(log_level, format, ...)            \
+    do {                                                           \
+        EMDEVIF_LOG(log_level, format __VA_OPT__(, ) __VA_ARGS__); \
     } while (0)
 #endif
 
@@ -35,18 +35,18 @@ TEST_SUIT(LoggerTest)
     TEST_CASE_BEGIN(BasicTest)
     {
 #line 1 EMDEVIF_LOGGER_TEST_FILE_NAME
-        EMDEVIF_LOGGER_TEST_LOG(
-            "0          [Info] An Info Message. (at Logger_test.cpp:1 in function `emdevif_test__LoggerTest__')\r\n",
-            emdevif::LogLevel::Info,
-            "An Info Message.");
+        EMDEVIF_LOGGER_TEST_LOG(emdevif::LogLevel::Info, "An Info Message.");
 
 #line 1437 EMDEVIF_LOGGER_TEST_FILE_NAME
-        EMDEVIF_LOGGER_TEST_LOG(
+        EMDEVIF_LOGGER_TEST_LOG(emdevif::LogLevel::Warning, "An Warning Message with integer num %d.", 114514);
+
+        emdevif::Thread::delay(emdevif::Thread::msToTick(2));
+
+        constexpr auto expect_log_out =
+            "0          [Info] An Info Message. (at Logger_test.cpp:1 in function `emdevif_test__LoggerTest__')\r\n"
             "1          [Warning] An Warning Message with integer num 114514. (at Logger_test.cpp:1437 in function "
-            "`emdevif_test__LoggerTest__')\r\n",
-            emdevif::LogLevel::Warning,
-            "An Warning Message with integer num %d.",
-            114514);
+            "`emdevif_test__LoggerTest__')\r\n";
+        EXPECT_STREQ(::emdevif::user_declares::logger::getBuffer(), expect_log_out);
     }
     TEST_CASE_END();
 }
