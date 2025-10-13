583a6fe9249e7bdd78cd2b7e2428b4a7ec4c2900
DuYicheng <duyicheng741@outlook.com>
2025-10-13 22:27:36 +0800

    simplify_decl_macros.hpp 增加“声明为零例模式”宏

--- core/inc/emdevif/simplify_decl_macros.hpp
+++ core/inc/emdevif/simplify_decl_macros.hpp
@@ -14,7 +14,7 @@
  * @param ClassName 待删除的类名
  */
 #define EMDEVIF_DELETE_COPY_CONSTRUCTOR(ClassName) \
-    ClassName(const ClassName&) = delete;        \
+    ClassName(const ClassName&) = delete;          \
     ClassName& operator=(const ClassName&) = delete
 
 /**
@@ -22,7 +22,7 @@
  * @param ClassName 待删除的类名
  */
 #define EMDEVIF_DELETE_MOVE_CONSTRUCTOR(ClassName) \
-    ClassName(ClassName&&) = delete;             \
+    ClassName(ClassName&&) = delete;               \
     ClassName& operator=(const ClassName&&) = delete
 
 /**
@@ -32,5 +32,14 @@
 #define EMDEVIF_DELETE_COPY_AND_MOVE(ClassName) \
     EMDEVIF_DELETE_COPY_CONSTRUCTOR(ClassName); \
     EMDEVIF_DELETE_MOVE_CONSTRUCTOR(ClassName)
+
+/**
+ * 声明为零例模式
+ * @param ClassName 类名
+ */
+#define EMDEVIF_DECLARE_ZERO_INSTANCE(ClassName) \
+    ClassName() = delete;                        \
+    ~ClassName() = delete;                       \
+    EMDEVIF_DELETE_COPY_AND_MOVE(ClassName)
 
 #endif  // !EMDEVIF_SIMPLIFY_DECL_HPP
