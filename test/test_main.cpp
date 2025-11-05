963c7a029096d4114ff6299b8078a18725a7ab35
DuYicheng <duyicheng741@outlook.com>
2025-11-05 17:09:28 +0800

    修改测试执行的方式，增加列表以便于选择忽略部分测试

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -5,27 +5,51 @@
  * @brief 测试主函数
  */
 
+#include <array>
+#include <utility>
+#include <string_view>
+
 #include "test_main.hpp"
 
 #include "heap_usage_checker.hpp"
 
+#ifndef EMDEVIF_TEST_IGNORE_NAME_LIST
+#define EMDEVIF_TEST_IGNORE_NAME_LIST {""}
+#endif
+
+#define TEST_LIST_ARG(func_name) #func_name, func_name
+
+static constexpr auto emdevif_test_list =
+    std::to_array<std::pair<std::string_view, void (*)()>>({{TEST_LIST_ARG(cFeatureTest)},
+                                                            {TEST_LIST_ARG(endianTest)},
+                                                            {TEST_LIST_ARG(typeTraitsTest)},
+                                                            {TEST_LIST_ARG(logSystemHeapUsage)},
+                                                            {TEST_LIST_ARG(initOnceTest)},
+                                                            {TEST_LIST_ARG(bitIntTest)},
+                                                            {TEST_LIST_ARG(ringBufferTest)},
+                                                            {TEST_LIST_ARG(loggerTest)},
+                                                            {TEST_LIST_ARG(heapTest)},
+                                                            {TEST_LIST_ARG(threadAndMutexTest)},
+                                                            {TEST_LIST_ARG(logSystemHeapUsage)}});
+
+static constexpr auto emdevif_test_ignore_name_list = std::to_array<std::string_view>(EMDEVIF_TEST_IGNORE_NAME_LIST);
+
+static constexpr bool inIgnoreNameList(const std::string_view& name) noexcept
+{
+    for (const auto& e : emdevif_test_ignore_name_list) {
+        if (e == name) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 extern "C" void emdevif_testEntry(void)
 {
-    cFeatureTest();
-
-    endianTest();
-    typeTraitsTest();
-
-    logSystemHeapUsage();
-
-    initOnceTest();
-    bitIntTest();
-    ringBufferTest();
-
-    loggerTest();
-
-    heapTest();
-    threadAndMutexTest();
-
-    logSystemHeapUsage();
+    for (const auto& [test_name, test_func] : emdevif_test_list) {
+        if (!inIgnoreNameList(test_name)) {
+            test_func();
+        }
+    }
 }
