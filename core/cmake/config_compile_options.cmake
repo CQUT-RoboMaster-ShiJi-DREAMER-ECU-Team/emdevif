4f416d8d983904be92f1e426694673218ede9b0c
DuYicheng <duyicheng741@outlook.com>
2025-11-07 12:48:04 +0800

    增加能够更改编译选项的缓存变量

--- core/cmake/config_compile_options.cmake
+++ core/cmake/config_compile_options.cmake
@@ -1,12 +1,21 @@
 
 add_library(emdevif_compile_options INTERFACE)
 
+set(EMDEVIF_MSVC_COMPILE_OPTIONS "/W4;/source-charset:utf-8" CACHE STRING
+    "Set to config compile options if this project use MSVC compiler. Default value is \
+    \"/W4;/source-charset:utf-8\"."
+)
+set(EMDEVIF_GNU_COMPILE_OPTIONS  "-Wall;-Wextra;-Wpedantic" CACHE STRING
+    "Set to config compile options if this project use GNU compiler. Default value is \
+    \"-Wall;-Wextra;-Wpedantic\"."
+)
+
 if (MSVC)
     target_compile_options(emdevif_compile_options INTERFACE
-        /W4 /source-charset:utf-8 /execution-charset:gb2312
+        ${EMDEVIF_MSVC_COMPILE_OPTIONS}
     )
 else ()
     target_compile_options(emdevif_compile_options INTERFACE
-        -Wall -Wextra -Wpedantic
+        ${EMDEVIF_GNU_COMPILE_OPTIONS}
     )
 endif ()
