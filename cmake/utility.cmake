0173f205dd4fbfa8eaab6e9003bddc752caa0e69
DuYicheng <duyicheng741@outlook.com>
2025-11-08 17:07:40 +0800

    给 CMake 函数加注释

--- cmake/utility.cmake
+++ cmake/utility.cmake
@@ -1,4 +1,15 @@
+#[==[
+# @file utility.cmake
+# @brief CMake 实用功能
+]==]
 
+#[==[
+# @function emdevif_trueOptionsCount(<out_result> ...)
+#
+# 用于获取给定的布尔类型变量中，真值的数量
+# @param[out] out_result 返回值为真的变量数
+# @param ... 待计数的变量。可以有多个，用空格分隔。
+]==]
 function(emdevif_trueOptionsCount out_result)
     set(trueCount 0)
 
@@ -11,6 +22,12 @@ function(emdevif_trueOptionsCount out_result)
     set(${out_result} ${trueCount} PARENT_SCOPE)
 endfunction()
 
+#[==[
+# @function emdevif_assertMutuallyExclusiveOptions(...)
+#
+# 用于断言给定的布尔类型变量中，真值的数量最多 1 个
+# @param ... 待断言的变量。可以有多个，用空格分隔。
+]==]
 function(emdevif_assertMutuallyExclusiveOptions)
     emdevif_trueOptionsCount(result ${ARGN})
 
